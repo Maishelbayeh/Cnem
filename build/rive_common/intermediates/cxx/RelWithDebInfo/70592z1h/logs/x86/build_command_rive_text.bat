ad08e042e823527c21927f77580aa78d0e15aef2
maishelbayeh <mayshelbayeh5@gmail.com>
2024-05-23 14:12:15 +0300

    with build gradle

---
+++ build/rive_common/intermediates/cxx/RelWithDebInfo/70592z1h/logs/x86/build_command_rive_text.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\maysh\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\maysh\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.8\\android\\.cxx\\RelWithDebInfo\\70592z1h\\x86" ^
+  rive_text
