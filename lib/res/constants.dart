daffa97cca6ddbc323367c97d63642cd7d3c2fc2
maishelbayeh <mayshelbayeh5@gmail.com>
2024-06-04 12:14:53 +0300

    delete

--- lib/res/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color.fromARGB(255, 0, 0, 0);
-const secondaryColor = Color.fromARGB(255, 10, 10, 10);
-const darkColor = Color(0xFF191923);
-const bodyTextColor = Color.fromARGB(255, 20, 20, 20);
-const bgColor = Color.fromARGB(255, 244, 245, 246);
-const defaultPadding = 20.0;
